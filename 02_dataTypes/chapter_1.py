0e8c70139aed9f4cdb7f5561b25ee15fd399cad0
Aparna Kumari <aparna.61085083@ltimindtree.com>
2026-08-11 14:26:33 +0530

    updating code

---
+++ 02_dataTypes/chapter_1.py
@@ -0,0 +1,7 @@
+sugar_amount =2
+print(f"Initial sugar value:  {sugar_amount}")
+sugar_amount = 22
+print(f"changed sugar value is : {sugar_amount}")
+
+print(f"id of 2: {id(2)}")
+print(f"id of 12: {id(12)}")
